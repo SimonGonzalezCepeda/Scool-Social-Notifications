9a9be6cc752682385d6c090797cc46fea18fcf54
SimonGonzalezCepeda <simongonzalez@iesebre.com>
2016-11-22 16:50:32 +0100

    Adding a test with assertions

---
+++ tests/MisTesstosTest.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: sylver
+ * Date: 22/11/16
+ * Time: 16:17
+ */
+class MisTesstosTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * It Shows Submodules
+     * @test
+     * @return string
+     */
+    public function it_shows_submodules()
+    {
+        // Preparacio del test
+        // Execucio
+        // Assertions o comprobacions
+        $this->assertTrue(true);
+    }
+}
